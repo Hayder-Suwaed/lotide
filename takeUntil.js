837b72714f78e021da5f278c8fe5d77a443bf300
Hayder-Suwaed <haiderhachim87@yahoo.com>
2019-12-19 23:10:07 +0000

    Implement takeUntil function to Lotide

---
+++ takeUntil.js
@@ -0,0 +1,53 @@
+/*
+Task: Create our own version of Lodash's takeWhile function
+Our takeUntil function will take two parameters:
+1. The array to work with
+2. The callback (which Lodash calls "predicate")
+3. Return a "slice of the array with elements taken from the
+    beginning"
+4. It should keep going until the callback/predicate
+    returns a truthy value
+*/
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`😁😁😁, Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎👎, Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  const results = [];
+
+  for (let x = 0; x < array.length; x++) {
+    // loops through the values of the given array
+    if (callback(array[x])) {
+      return results;
+    }
+    results.push(array[x]);
+  }
+};
+
+assertArraysEqual(
+  takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5], (x) => x < 0),
+  [1, 2, 5, 7, 2]
+);
+assertArraysEqual(
+  takeUntil(
+    ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"],
+    (x) => x === ","
+  ),
+  ["I've", "been", "to", "Hollywood"]
+);
