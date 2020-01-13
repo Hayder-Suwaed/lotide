dff0dc0cf616f93f52ed9a7664f7fe55a9b3f14d
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-01-13 16:58:53 -0700

    exporting all functions to index file

---
+++ index.js
@@ -0,0 +1,36 @@
+const countLetters = require ('./countLetters');
+const countonly = require ('./countonly');
+const eqArrays  = require ('./eqArrays');
+const eqobjects  = require ('./eqobjects');
+const findKeyByValue   = require ('./findKeyByValue');
+const flatten = require ('./flatten');
+const head  = require ('./head');
+const index = require ('./index');
+const letterPositions = require ('./letterPositions');
+const map = require ('./map');
+const reverse = require ('./reverse');
+const takeUntil =require ('./takeUntil');
+const without = require ('./without');
+const tail  = require ('./tail')
+const middle = require ('./middle')
+
+module.exports = {
+  
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  countLetters: countLetters,
+  countonly: countonly,
+  eqArrays: eqArrays,
+  eqobjects: eqobjects,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  head: head,
+  index: index,
+  letterPositions: letterPositions,
+  map: map,
+  middle: middle,
+  reverse: reverse,
+ takeUntil: takeUntil,
+ without: without,
+};
