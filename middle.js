98b7594733c75c1274ce83428d6d3527eb6b4d1a
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-01-13 16:50:31 -0700

    refactored

--- middle.js
+++ middle.js
@@ -1,51 +1,24 @@
+const eqArrays = require('./eqArrays')
 const middle = function(array) {
- var result = []
+  let result = []
 if (array.length <= 2 ){
-result = [];
+ result = [] 
 }
 else if (array.length % 2 === 0 && array.length >2)
 {
-result.push(array[(array.length /2)-1])
-result.push( array[array.length/2]);
+ result.push(array[(array.length /2)-1]) 
+ result.push( array[array.length/2]);
 }
 else if (array.length === 3)
 {
-result.push(array[1])
+ result.push(array[1])
 }
-else
+else 
 {
- result.push(array[Math.floor(array.length/2)])
+  result.push(array[Math.floor(array.length/2)])
 }
 return result
 }
-const eqArrays = function(a , b ) {
- if (a.length !== b.length){
- return false
- }
- else {
- for (var i = 0; i < a.length; i++) {
- if (a[i] !== b[i]) {
- return false;
- }}
-return true
-}}
-const
-assertArraysEqual = function(actual, expected) {
- const match = eqArrays(actual,expected)
- if ( match) {
- console.log(`Assertion Passed: ${actual} === ${expected}`);
- }
- else {
- console.log(`Asseration Passed: ${actual} !== ${expected}`);
- }
- return actual;
-};
 
+module.exports = middle;
 
-
-
-
-
-
-assertArraysEqual(middle([1, 2, 4, 3, 4]), [4]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]) // => [3, 4] 
