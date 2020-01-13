325ea0f57bb997c67f02cdbe497ed91b127ba7da
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-01-13 16:46:54 -0700

    refactored

--- tail.js
+++ tail.js
@@ -1,17 +1,5 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ?
-    console.log(`\u2705 Assertion Passed: "${actual}" === "${expected}"`) :
-    console.log(`\uD83D\uDD34 Assertion Failed: "${actual}" !== "${expected}"`);
-};
-
-module.exports = assertEqual;
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-const tail = function(arr) {
-  return arr.length > 2 ? arr.slice(1) : [];
-};
-
+const assertEqual = require("./assertEqual");
+function tail(array) {
+  return array.slice(1);
+}
 module.exports = tail;
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
