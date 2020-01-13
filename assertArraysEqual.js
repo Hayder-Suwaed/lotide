ca48d357dbbe47ee48b1d8f337549aea188bfd29
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-01-13 16:49:37 -0700

    refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,14 @@
-const eqArrays = function(a, b) {
-  if (a.length !== b.length) {
-    return false;
-  } else {
-    for (var i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        return false;
-      }
-    }
-    return true;
+const eqArrays = require('./eqArrays');
+  const assertArraysEqual = function(array1, array2) {
+  const match = eqArrays(array1,array2)
+  if ( match) {
+  console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
   }
-};
-const assertArraysEqual = function(array1, array2) {
-  const match = eqArrays(array1, array2);
-  if (match) {
-    console.log(`Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`Asseration Passed: ${array1} !== ${array2}`);
+  else {
+  console.log(`🛑🛑🛑Asseration Failed: ${array1} !== ${array2}`);
   }
   return array1;
-};
+  };
+  
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+module.exports = assertArraysEqual;
